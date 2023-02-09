6b3cf664487287b058caf6004b39667a001bf759
Gotchinomics <gotchinomics@gmail.com>
2023-02-09 21:41:41 +0100

    Added figure displaying number of channelings per day in the snapshot

--- kinship_analysis.m
+++ kinship_analysis.m
@@ -5,9 +5,12 @@ close all
 load channeling_events
 
 % Loading data from the dynamic table into regular vectors
-gotchis = channeling_events.Gotchi_id;
-parcels = channeling_events.Realm_id;
+gotchis = channelingevents.Gotchi_id;
+parcels = channelingevents.Realm_id;
 FUD = channelingevents.FUD;
+FOMO = channelingevents.FOMO;
+ALPHA = channelingevents.ALPHA;
+KEK = channelingevents.KEK;
 spillover = channelingevents.spilloverRate/10000;
 
 % Getting day of each channeling event
@@ -16,4 +19,10 @@ blockDate = string(blockDateTime(:,1:10));
 
 % Calculating kinship based on the amount of FUD produced per channeling
 kinship = 50*(FUD/(20)).^2;
+
+% Evaluating data, calculating events per day contained in the snapshot
+figure('Color','white')
+histogram(datetime(blockDate))
+ylabel('# channelings analysed')
+grid on
 
